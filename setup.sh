ee72e5ea8d585fcfe953ca017255c480e6917a53
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2025-08-04 13:13:36 +1000

    fixed bug with checking if in a tmux session

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ function _prompt_container_or_host() {
 PROMPT_COMMAND='PS1=\"\\[\\033[35m\\][\`uname -s -r\`] \\[\\033[32m\\]\\u \\[\\033[00m\\]@ \$\(_prompt_container_or_host\)\\[\\033[32m\\]\$\(__git_ps1\) \\[\\033[34m\\]\\w \\[\\033[00m\\]\\$ \"'
 
 if command -v tmux > /dev/null; then
-  if [ -z '\$TMUX' ]; then
+  if [ -z \$TMUX ]; then
     if tmux list-sessions > /dev/null 2>&1; then
       tmux attach-session -d || tmux new-session
     else
