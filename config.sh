af8034ba7c5b047e04d7026847bdd7c737ad1d71
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2025-08-18 15:30:42 +1000

    added btop configuration

--- config.sh
+++ config.sh
@@ -6,6 +6,8 @@ git clone git@github.com:lachlan-ikeguchi/alacritty ~/.config/alacritty
 
 git clone git@github.com:lachlan-ikeguchi/htop ~/.config/htop
 
+git clone git@github.com:Lachlan-Ikeguchi/btop ~/.config/btop
+
 git clone git@github.com:lachlan-ikeguchi/tmux ~/.config/tmux
 mkdir ~/.config/tmux/plugins
 mkdir ~/.config/tmux/plugins/tpm
