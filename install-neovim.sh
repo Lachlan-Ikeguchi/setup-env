42c6f7c62a7d99b5cb1cef5b768f595d5e34df0f
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-03-08 12:45:52 +1000

    refactor(install-neovim): split the neovim install part into a seperate script

---
+++ install-neovim.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:neovim/neovim.git ~/neovim
+cd ~/neovim/
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make CMAKE_BUILD_TYPE=Release
+make install
+export PATH="$HOME/neovim/bin:$PATH"
+cd ~
