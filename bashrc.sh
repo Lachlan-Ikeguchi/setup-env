2aa83277baa85c68081e8d0790d6f092ca0a274c
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-06-23 22:16:59 +1000

    fix(bashrc): fixed update from ~/.scripts to ~/bin in .bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -1,5 +1,5 @@
 echo "# ---------- added by script ---------- #
-source ~/.scripts/git-prompt.sh
+source ~/bin/git-prompt.sh
 
 function _prompt_container_or_host() {
   if [[ -n \"\$CONTAINER_ID\" ]]; then
